e65f1037c1c43d74c1fc38e5d8081b892d7d4d95
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-03-14 11:38:33 +0700

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+
+    private array $register = [];
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        foreach ($this->register as $index => $value) $this->app->bind($index, $value);
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
