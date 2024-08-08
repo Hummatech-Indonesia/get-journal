7f59fdb6d89d4e378793d2d3586d753be2fb6ca6
Candra Andika Putra <88470704+Candrandika@users.noreply.github.com>
2024-08-08 15:24:58 +0700

    mod: add datatable of list transaction

--- resources/views/pages/users/transactions/detail.blade.php
+++ resources/views/pages/users/transactions/detail.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.auth')
 
 @section('title', 'Detail Transaksi')
-    @include('components.swal')
 
 @section('content')
+    @include('components.swal')
+    <div class="card">
+        <div class="card-body">
+            Detail Transaksi
+        </div>
+    </div>
+    <div class="d-flex gap-3 mt-5">
+        <button class="btn btn-sm btn-dark d-flex gap-2 justify-content-center align-items-center"><i class="ki-duotone ki-double-left fs-1"><span class="path1"></span><span class="path2"></span></i> Kembali</button>
+        <a href="#" class="btn btn-sm btn-primary d-flex gap-2 justify-content-center align-items-center"><i class="ki-duotone ki-credit-cart fs-1"><span class="path1"></span><span class="path2"></span></i> Bayar</a>
+
+    </div>
 @endsection
--- resources/views/pages/users/transactions/index.blade.php
+++ resources/views/pages/users/transactions/index.blade.php
@@ -1,7 +1,16 @@
 @extends('layouts.auth')
 
 @section('title', 'Transaksi')
-    @include('components.swal')
-
 @section('content')
+    @include('components.swal')
+    @include('pages.users.transactions.widgets.dt-list-transactions')
 @endsection
+@push('style')
+    <link rel="stylesheet" href="{{asset('assets/plugins/custom/datatablesnet/datatables.min.css')}}">
+@endpush
+@push('script')
+    <script src="{{asset('assets/plugins/custom/number-format/number-format.js')}}"></script>
+    <script src="{{asset('assets/plugins/custom/datatablesnet/datatables.min.js')}}"></script>
+    <script src="{{asset('assets/plugins/custom/datatablesnet/pdfmake.min.js')}}"></script>
+    <script src="{{asset('assets/plugins/custom/datatablesnet/vfs_fonts.js')}}"></script>
+@endpush
