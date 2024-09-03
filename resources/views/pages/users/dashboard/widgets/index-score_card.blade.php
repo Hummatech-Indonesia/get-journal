076f3b4275d75c70d0b63c2b65c3aa596ced035f
putrapangestu <putraprp8@gmail.com>
2024-09-03 23:42:42 +0700

    DASHBOARD
    commit add data in score card

--- resources/views/pages/users/dashboard/widgets/index-score_card.blade.php
+++ resources/views/pages/users/dashboard/widgets/index-score_card.blade.php
@@ -3,7 +3,7 @@
         <div class="card card-dashboard  position-relative overflow-hidden">
             <div class="card-body p-5">
                 <h2 class="m-0">Guru</h2>
-                <div class="text-info fs-2tx fw-bold">10</div>
+                <div class="text-info fs-2tx fw-bold">{{ $data->teacher }}</div>
                 <i class="position-absolute opacity-25 ki-duotone ki-teacher text-info fs-6tx ">
                     <div class="path1"></div><div class="path2"></div>
                 </i>
@@ -14,7 +14,7 @@
         <div class="card card-dashboard  position-relative overflow-hidden">
             <div class="card-body p-5">
                 <h2 class="m-0">Siswa</h2>
-                <div class="text-primary fs-2tx fw-bold">10</div>
+                <div class="text-primary fs-2tx fw-bold">{{ $data->student }}</div>
                 <i class="position-absolute opacity-25 ki-duotone ki-briefcase text-primary fs-6tx ">
                     <div class="path1"></div><div class="path2"></div>
                 </i>
@@ -25,7 +25,7 @@
         <div class="card card-dashboard  position-relative overflow-hidden">
             <div class="card-body p-5">
                 <h2 class="m-0">Kelas</h2>
-                <div class="text-success fs-2tx fw-bold">10</div>
+                <div class="text-success fs-2tx fw-bold">{{ $data->classroom }}</div>
                 <i class="position-absolute opacity-25 ki-duotone ki-book-open text-success fs-6tx ">
                     <div class="path1"></div><div class="path2"></div><div class="path3"></div><div class="path4"></div>
                 </i>
@@ -36,7 +36,7 @@
         <div class="card card-dashboard  position-relative overflow-hidden">
             <div class="card-body p-5">
                 <h2 class="m-0">Sisa Premium</h2>
-                <div class="text-warning fs-2tx fw-bold">10</div>
+                <div class="text-warning fs-2tx fw-bold">{{ $data->premium }}</div>
                 <i class="position-absolute opacity-25 ki-duotone ki-medal-star text-warning fs-6tx ">
                     <div class="path1"></div><div class="path2"></div><div class="path3"></div><div class="path4"></div>
                 </i>
