ff8e315f9b459009693b6950af9017b2fe24d98e
putrapangestu <putraprp8@gmail.com>
2024-09-26 10:51:49 +0700

    LOGIN
    Commit change login

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -13,10 +13,10 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        dd($request, $request->expectsJson);
         if (strpos($request->url(), "/api/") !== false) {
             return $request->expectsJson() ? null : route('unauthorized');
         } else {
+            dd($request, $request->expectsJson(), $request->all(), auth()->user());
             // if(auth()->user()) {
             //     $roles = auth()->user->roles->pluck('name')->toArray();
             //     if (in_array('admin',$roles) || in_array('school', $roles)) {
